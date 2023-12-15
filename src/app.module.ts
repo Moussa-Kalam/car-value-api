b42119e2570068314dd38a861b89d41fb1ebfed1
Moussa-Kalam <m.amzat@alustudent.com>
2023-12-15 17:21:19 +0200

    chore: generate modules, controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReportsService } from './reports/reports.service';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ReportsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ReportsService, UsersService],
 })
 export class AppModule {}
