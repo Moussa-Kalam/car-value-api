6000b01c5a1be765c175727b2ba69e8e9192c8eb
Moussa-Kalam <m.amzat@alustudent.com>
2023-12-17 19:33:01 +0200

    feat: add logic to update user data

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -22,4 +22,17 @@ export class UsersService {
   find(email: string) {
     return this.repo.find({ where: { email } });
   }
+
+  async update(id: number, attrs: Partial<User>) {
+    // Get the user with the given id from the db
+    const user = await this.findOne(id);
+
+    if (!user) throw new Error('User not found');
+
+    // Update the user object with input data
+    Object.assign(user, attrs);
+
+    // Save the updated user object to the db
+    return this.repo.save(user);
+  }
 }
