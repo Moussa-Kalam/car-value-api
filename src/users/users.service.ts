760dc9c326d40d0f29e5ca984af9b4f92ac75f79
Moussa-Kalam <m.amzat@alustudent.com>
2023-12-17 18:47:21 +0200

    docs: add comments to explain code

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,8 +8,10 @@ export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
   create(email: string, password: string) {
+    // Instance of User Entity
     const user = this.repo.create({ email, password });
 
+    // Persist the user in db
     return this.repo.save(user);
   }
 }
