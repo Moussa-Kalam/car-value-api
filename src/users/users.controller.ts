d2024ee7d7261ba520df13efd39d5c8d8769753e
Moussa-Kalam <m.amzat@alustudent.com>
2023-12-18 21:20:21 +0200

    feat: add custom data serialization to format outgoing responses

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,22 @@
 import {
   Body,
   Controller,
-  Get,
-  Post,
-  Param,
-  Query,
   Delete,
-  Patch,
+  Get,
   NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
 } from '@nestjs/common';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
-import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
