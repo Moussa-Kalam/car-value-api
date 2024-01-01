d2ab04101821df4032db4007002bf4c4e5d9736f
Moussa-Kalam <m.amzat@alustudent.com>
2024-01-01 20:20:13 +0200

    feat: notify the handlers about the currently signed in user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,11 +11,13 @@ import {
   Session,
 } from '@nestjs/common';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user-decorator';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
-import { AuthService } from './auth.service';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -26,8 +28,8 @@ export class UsersController {
   ) {}
 
   @Get('/whoami')
-  whoAmI(@Session() session: any) {
-    return this.usersService.findOne(session.userId);
+  whoAmI(@CurrentUser() user: User) {
+    return user;
   }
 
   @Post('/signout')
